678ba1481418627f0e769900b16ee8312d037dc8
meerkenia <132111009+meerkenia@users.noreply.github.com>
2023-06-27 17:17:37 -0400

    Create icebreaker.py

---
+++ icebreaker.py
@@ -0,0 +1,14 @@
+"""This is a short icebreaker program"""
+
+name = input("Name = ")
+age = input("Age = ")
+school = input("School = ")
+fave_movie = input("Favorite movie = ")
+fave_sports_team = input("Favorite sports team = ")
+fave_snack = input("Favorite snack = ")
+
+icebreaker = f"Hi, my name is {name}. I am {age} years old. I am a student at {school}. \
+My favorite movie is {fave_movie}. My favorite sports team is the {fave_sports_team}. My favorite snack is {fave_snack}. \
+I am very excited to be here and to meet you all!"
+
+print(icebreaker)
